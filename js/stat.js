d427fff4f2e98675517d1f72bb1e8133888ee1ba
Yulia Maximova <juliam2007@mail.ru>
2017-04-02 19:51:45 +0300

    Подключен stat.js в файле index.html. В файле stat.js настроен вывод вертикальной гистограммы, а также вывод имен игроков и их времени

---
+++ js/stat.js
@@ -0,0 +1,65 @@
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.shadowOffsetX = 10; 
+  ctx.shadowOffsetY = 10; 
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)'; 
+  ctx.fillStyle = 'white';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.shadowOffsetX = 0; 
+  ctx.shadowOffsetY = 0;
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+  
+  var max = -1;
+  var maxIndex = -1;
+
+  for (var i = 0 ; i < times.length; i++) {
+    var time = times[i];    
+    if (time > max) {
+      max = time;
+      maxIndex = i;
+    }
+  }  
+
+  var histogramHeight = 150;
+  var step = histogramHeight / max;
+
+  ctx.fillText('Список результатов:', 120, 60);  
+
+  var barHeigth = 40; // px;
+  var indent = 90;    // px;
+  var initialX = 140; // px;
+  var initialY = 250;  // px;
+  var indentName = 5;  // px;
+  var indentTime = 25; // px;
+  
+  ctx.textBaseline = 'top'; // положение надписи от левого верхнего угла
+  
+  for (var i = 0; i < times.length; i++) {
+
+    var barWidth = times[i] * step;
+
+    // Задаем цвета гистограмм для игроков
+    
+    function getBarColor (namePlayer, randomOpacity) {
+    var randomOpacity = Math.random().toFixed(2); // Переменная, задающая прозрачность колоники гистограмма
+    var namePlayer = names[i];
+      if (namePlayer == 'Вы') {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        ctx.fillStyle = 'rgba(0, 0, 255, '+randomOpacity+')';
+      }
+    }
+
+    ctx.fillStyle = getBarColor(); // Вызываем функцию заливки гистограммы
+    ctx.fillRect(initialX + indent * i, initialY - barWidth, barHeigth, barWidth);
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillText(names[i], initialX + indent * i, initialY + indentName);
+    ctx.fillText(times[i].toFixed(0), initialX + indent * i, initialY - barWidth - indentTime);
+  }
+};
