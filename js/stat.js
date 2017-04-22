3e5c0f6837c72d0f1649af0121b30c3407e698be
Yulia Maximova <juliam2007@mail.ru>
2017-04-22 18:42:30 +0300

    Правки тревиса

--- js/stat.js
+++ js/stat.js
@@ -14,33 +14,24 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура вы победили!', 120, 40);
-
+  
   function getMaxValue(array) {
-    var max = -1;
-    for (var i = 0; i < array.length; i++) {
-      var value = array[i];
-      if (value > max) {
-        max = value;
-        maxIndex = i;
+      var max = -1;
+      var maxIndex = -1;
+      for (var i = 0 ; i < array.length; i++) {
+        var value = array[i];
+        if (value > max) {
+          max = value;
+          maxIndex = i;
+        }
       }
-    }
-    return max;
-  }
-
-  // Задаем цвета гистограмм для игроков
-  function fillBarColor(namePlayer) {
-  var randomOpacity = Math.random().toFixed(2); // Переменная, задающая прозрачность колоники гистограмма    
-    if (namePlayer == 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, '+randomOpacity+')';
-    }
-  }
+      return max;
+    } 
 
   var histogramHeight = 150;
-  var step = histogramHeight / getMaxValue(times[i]);
+  var step = histogramHeight / getMaxValue(times);
 
-  ctx.fillText('Список результатов:', 120, 60);
+  ctx.fillText('Список результатов:', 120, 60);  
 
   var barHeigth = 40; // px;
   var indent = 90;    // px;
@@ -53,6 +44,17 @@ window.renderStatistics = function (ctx, names, times) {
 
   for (var i = 0; i < times.length; i++) {
     var barWidth = times[i] * step;
+
+    // Задаем цвета гистограмм для игроков    
+    function fillBarColor (namePlayer) {
+    var randomOpacity = Math.random().toFixed(2); // Переменная, задающая прозрачность колоники гистограмма    
+      if (namePlayer === 'Вы') {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        ctx.fillStyle = 'rgba(0, 0, 255, '+randomOpacity+')';
+      }
+    }
+
     ctx.fillStyle = fillBarColor(names[i]); // Вызываем функцию заливки гистограммы
     ctx.fillRect(initialX + indent * i, initialY - barWidth, barHeigth, barWidth);
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
