5c0a316801183e2edb1c84b7780f56e5f745d1b8
Yulia Maximova <juliam2007@mail.ru>
2017-04-22 18:21:54 +0300

    Правки тревиса

--- js/stat.js
+++ js/stat.js
@@ -1,38 +1,37 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
-  ctx.shadowOffsetX = 10; 
-  ctx.shadowOffsetY = 10; 
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)'; 
+window.renderStatistics = function(ctx, names, times) {
+  ctx.shadowOffsetX = 10;
+  ctx.shadowOffsetY = 10;
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
   ctx.fillStyle = 'white';
   ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(100, 10, 420, 270);
 
-  ctx.shadowOffsetX = 0; 
+  ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура вы победили!', 120, 40);
   
-
   function getMaxValue(array) {
-      var max = -1;
-      var maxIndex = -1;
-      for (var i = 0 ; i < array.length; i++) {
-        var value = array[i];
-        if (value > max) {
-          max = value;
-          maxIndex = i;
-        }
-      }
-      return max;
-    } 
+    var max = -1;
+    var maxIndex = -1;
+    for (var i = 0 ; i < array.length; i++) {
+    var value = array[i];
+    if (value > max) {
+      max = value;
+      maxIndex = i;
+    }
+  }
+  return max;
+}
 
   var histogramHeight = 150;
   var step = histogramHeight / getMaxValue(times[i]);
 
-  ctx.fillText('Список результатов:', 120, 60);  
+  ctx.fillText('Список результатов:', 120, 60);
 
   var barHeigth = 40; // px;
   var indent = 90;    // px;
@@ -44,12 +43,11 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.textBaseline = 'top'; // положение надписи от левого верхнего угла
   
   for (var i = 0; i < times.length; i++) {
-
     var barWidth = times[i] * step;
 
-    // Задаем цвета гистограмм для игроков
+// Задаем цвета гистограмм для игроков
     
-    function fillBarColor (namePlayer) {
+    function fillBarColor(namePlayer) {
     var randomOpacity = Math.random().toFixed(2); // Переменная, задающая прозрачность колоники гистограмма    
       if (namePlayer == 'Вы') {
         ctx.fillStyle = 'rgba(255, 0, 0, 1)';
